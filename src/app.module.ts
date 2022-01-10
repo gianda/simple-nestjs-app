59371532f90985c321216e2270fee71234651c60
Gianluca Marino <gmarino@athics.eu>
2022-01-10 16:27:51 +0100

    implementato subscribe rabbitmq

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 
 @Module({
-  imports: [],
+  imports: [
+    RabbitMQModule.forRoot(RabbitMQModule, {
+      exchanges: [
+        {
+          name: 'test-exchange',
+          type: 'topic',
+        },
+      ],
+      uri: 'amqp://rabbit:rabbit@queue:5672',
+    }),
+    AppModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
+import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  @RabbitSubscribe({
+    exchange: 'test-exchange',
+    routingKey: 'test-routing',
+    queue: 'test-messages',
+  })
+  public async pubSubHandler(msg: {}) {
+    console.log(`Received message: ${JSON.stringify(msg)}`);
+  }
+
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World 3001!';
   }
 }
